57162a50c3060d8bedbf3f64098ffb3330cf7c3b
NicolasCodeCraft <kunicolas2018@gmail.com>
2025-10-25 21:34:49 +0200

    Insert multiple customers into the Customers table
    Added a USE statement for the alx_book_store database before inserting multiple rows into the Customers table.

--- task_6.sql
+++ task_6.sql
@@ -1,6 +1,8 @@
--- Insert multiple rows into the Customers table
+-- Insert multiple rows into the customers table in the alx_book_store database
 
-INSERT INTO Customers (customer_id, customer_name, email, address)
+USE alx_book_store;
+
+INSERT INTO customers (customer_id, customer_name, email, address)
 VALUES
 (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
 (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
